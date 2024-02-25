a5b8da6a6f34f41dd3d6551c9a0d1740f8071b13
Enriquen <146202992+Enriiquecm@users.noreply.github.com>
2024-02-25 19:59:28 +0100

    Juego de Cartas Terminado

---
+++ Mano.java
@@ -0,0 +1,17 @@
+
+public class Mano {
+  ////Atributos
+  private double puntuacionMano ;
+  private Carta manoJugador;
+
+
+  ////Métodos
+  public Mano(double puntuacionMano, Carta manoJugador) {
+    this.puntuacionMano = puntuacionMano;
+    this.manoJugador = manoJugador;
+  }
+
+  public void obtieneCarta(){
+    
+  }
+}
